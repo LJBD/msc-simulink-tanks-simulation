e905e85c708dfe97505552664dc58fce2f809300
LJBD <restinginpeace@interia.pl>
2017-03-28 19:00:58 +0200

    Added a model of tanks.'

---
+++ simulink_model/tanks_runner.m
@@ -0,0 +1,20 @@
+% Geometry constants
+a = 31;
+b = 40;
+c = 10;
+R = 40;
+w = 5;
+h_max = 40;
+
+% Starting point
+h10 = 0;
+h20 = 0;
+h30 = 0;
+
+% Simulation time
+T = 10;
+
+% Outflow constats -> TODO: get them from experiments
+C1 = 1;
+C2 = 1;
+C3 = 1;

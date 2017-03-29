e7dd7caecccbecbaff7e545ef5bcda4c95d523f3
LJBD <restinginpeace@interia.pl>
2017-03-29 16:11:36 +0200

    Settings where both tanks 1 and 2 get depleted before control is applied at 20 s.

--- simulink_model/tanks_runner.m
+++ simulink_model/tanks_runner.m
@@ -7,14 +7,14 @@ w = 5;
 h_max = 40;
 
 % Starting point
-h10 = 3;
-h20 = 0.10;
-h30 = 30;
+h10 = 0.1;
+h20 = 0.1;
+h30 = 7;
 
 % Simulation time
 T = 40;
 
 % Outflow constats -> TODO: get them from experiments
 C1 = 10;
-C2 = 10;
+C2 = 30;
 C3 = 10;
